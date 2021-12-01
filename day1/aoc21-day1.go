ab10318e7e9a1dc10ba6a3e5fe9b75a6b7c37fdf
Timo Traulsen <Timo.Traulsen@deutschebahn.com>
2021-12-01 22:54:04 +0100

    add first day of aoc

---
+++ day1/aoc21-day1.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "io/ioutil"
+	"strconv"
+  "os"
+	"strings"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func task1 (fileName string) {
+	pwd, _ := os.Getwd()
+	f, err := os.Open(pwd + "/" + fileName)
+  check(err)
+
+	scanner := bufio.NewScanner(f)
+
+	var lastLine int64 = -1
+	var currentLine int64 = -1
+	var increments = 0
+
+	for scanner.Scan() {
+		var line = scanner.Text()
+		currentLine,_ = strconv.ParseInt(line,10,64)
+		//fmt.Println(line)
+		if(lastLine != -1){
+			if(lastLine < currentLine) {
+				increments++
+			}
+		}
+		lastLine = currentLine
+  }
+	fmt.Printf("Number of increments in %v: %v\n",fileName,increments)
+
+}
+
+func readCompleteFile(fileName string) []string{
+	pwd, _ := os.Getwd()
+
+	fileBytes, err := ioutil.ReadFile(pwd + "/" + fileName)
+	check(err)
+
+	return strings.Split(string(fileBytes), "\n")
+}
+
+func addElements(lines []string,offset int)int64{
+	i,_:= strconv.ParseInt(lines[offset],10,64)
+	j,_:= strconv.ParseInt(lines[offset+1],10,64)
+	k,_:= strconv.ParseInt(lines[offset+2],10,64)
+	return i+j+k
+}
+
+func task2 (fileName string) {
+	var lines = readCompleteFile(fileName)
+	var increments=0
+	for i:=0; i<len(lines)-3;i++ {
+		if(addElements(lines,i+1) > addElements(lines,i)){
+			increments++
+		}
+	}
+	fmt.Printf("Number of measured increments in %v: %v\n",fileName,increments)
+}
+
+func main() {
+	task1("testdata.txt")
+	task1("day1-inputdata-1.txt")
+	task2("testdata.txt")
+	task2("day1-inputdata-1.txt")
+}
